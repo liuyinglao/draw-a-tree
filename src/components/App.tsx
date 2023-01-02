3b24f23f75727dc02135311ef1e9cb61a5f45b66
Yinglao Liu <yinglao@fb.com>
2023-01-01 22:04:30 -0800

    use react-d3-tree library to create a tree

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import OrgChartTree from './react_d3_tree'
 
 function App() {
   return (
@@ -19,7 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <OrgChartTree />
     </div>
+    
   );
 }
 
